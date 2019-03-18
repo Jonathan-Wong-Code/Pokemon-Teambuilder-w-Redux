9bbd04e828dd17777e48834f154feb831ab4c7ae
Jonathan <jon.wong.music@gmail.com>
2019-03-18 16:42:13 -0400

    Fixed modal buttons with fixed heights

--- src/js/tests/integrations/pokeTeam-action-integrations.test.js
+++ src/js/tests/integrations/pokeTeam-action-integrations.test.js
@@ -9,7 +9,7 @@ const uid = 'test123';
 describe('the addPokeTeam action creator', () => {
   let store;
   beforeEach(async (done) => {
-    await database.ref(`test/${uid}`).set(pokeTeam.team3).then(() => done());
+    await database.ref(`test/${uid}/${pokeTeam.team3.id}`).set(pokeTeam.team3).then(() => done());
     const initialState = {
       savedTeams : {
         [pokeTeam.team3.id] : pokeTeam.team3
@@ -40,4 +40,42 @@ describe('the addPokeTeam action creator', () => {
     const newState = store.getState();
     expect(newState.savedTeams).toEqual({});
   });
+
+  it('should edit a pokemon team in redux and firebase', async () => {
+    const updatedTeam =  {
+      id : 'team3',
+      name : 'pokeTeam1edit',
+      description : 'This is pokeTeam1',
+      pokemon : [{
+        name : 'pokemona',
+        sprites : { front_default : 'sprite1' }
+      }, {
+        name : 'pokemonb',
+        sprites : { front_default : 'sprite2' }
+      }, {
+        name : 'pokemonc',
+        sprites : { front_default : 'sprite3' }
+      }, {
+        name : 'pokemond',
+        sprites : { front_default : 'sprite4' }
+      }, {
+        name : 'pokemone',
+        sprites : { front_default : 'sprite5' }
+      }, {
+        name : 'pokemonf',
+        sprites : { front_default : 'sprite6' }
+      }]
+    }
+
+    await store.dispatch(editPokeTeam(updatedTeam))
+    const newState = store.getState();
+    expect(newState.savedTeams).toEqual({
+      [pokeTeam.team3.id] : updatedTeam
+    });
+    
+    const snapshot = await database.ref(`test/${uid}`).updat('value');
+    expect(snapshot.val()).toEqual(updatedTeam);
+  });
+
+  
 })//end describe
