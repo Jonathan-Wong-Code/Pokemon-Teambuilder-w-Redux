886efe8cac0d84a7871bef4f539a52ca3388379b
Jonathan <jon.wong.music@gmail.com>
2019-03-24 13:41:16 -0400

    Refactored Confirm Save for Hooks

--- src/js/components/ConfirmSave.js
+++ src/js/components/ConfirmSave.js
@@ -1,42 +1,39 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-export class ConfirmSave extends React.Component {
-  constructor() {
-    super();
+const ConfirmSave = ({ handleToggleSaveModal, pokemon }) => {
+  const closeBtn = useRef(null);
 
-    this.closeBtn = React.createRef();
-  }
-
-  componentDidMount() {
-    this.closeBtn.current.focus();
-  }
+  useEffect(() => {
+    closeBtn.current.focus();
+  }, []);
   
-  render() {
-    return (
+  return (
+    <div 
+      className='modal__background' 
+      onClick={handleToggleSaveModal} 
+      data-test='confirm-save-component'
+    >
       <div 
-        className='modal__background' 
-        onClick={this.props.handleToggleSaveModal} 
-        data-test='confirm-save-component'
+        className='confirm-save__modal' 
+        onClick={(e) => e.stopPropagation()}
+        data-test='confirm-save-body'
       >
-        <div className='confirm-save__modal' onClick={(e) => e.stopPropagation()}
-          data-test='confirm-save-body'
+        <p data-test='confirm-save-text'>Saved <span className='confirm-save__name'>
+            {pokemon.name}
+          </span>
+        </p>
+        <button 
+          onClick={handleToggleSaveModal} 
+          className='btn confirm-save__btn'
+          data-test='confirm-save-close-btn'
+          ref={closeBtn}
         >
-          <p data-test='confirm-save-text'>Saved <span className='confirm-save__name'>
-              {this.props.pokemon.name}
-            </span>
-          </p>
-          <button 
-            onClick={this.props.handleToggleSaveModal} 
-            className='btn confirm-save__btn'
-            data-test='confirm-save-close-btn'
-            ref={this.closeBtn}
-          >
-          Close
-          </button>
-        </div>
+        Close
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 
 export default ConfirmSave;
