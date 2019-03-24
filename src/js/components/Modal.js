d5198cfbce64d4f20d7eb230dc6248bba17c1768
Jonathan <jon.wong.music@gmail.com>
2019-03-24 13:47:16 -0400

    Refacored Modal Component for hooks

--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -1,23 +1,117 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PokemonTypes from './PokemonTypes';
 
-export class Modal extends React.Component{
-  constructor(){
-    super();
+// export class Modal extends React.Component{
+//   constructor(){
+//     super();
 
-    this.pokeModalButton = React.createRef();
-  }
+//     this.pokeModalButton = React.createRef();
+//   }
 
-  componentDidMount (){
-    this.pokeModalButton.current.focus();
-  }
+//   componentDidMount (){
+//     this.pokeModalButton.current.focus();
+//   }
   
 
-  renderTypes = () => this.props.pokemon.types.map(type => {
+//   renderTypes = () => this.props.pokemon.types.map(type => {
+//     return <PokemonTypes type={type} key={type.type.name} />;
+//   });
+  
+//   renderAbilities = () => this.props.pokemon.abilities.map(ability => {
+//     return (
+//       <li 
+//         className='modal__ability' 
+//         key={ability.ability.name}
+//         data-test='modal-ability'
+//       > 
+//         {ability.ability.name} 
+//       </li>    
+//     );
+//   });
+
+//   renderStats = () => this.props.pokemon.stats.map(stat => (
+//     <li key={stat.stat.name} data-test='modal-stat'>
+//       <span className='modal__stat-name'>
+//         { stat.stat.name }:
+//       </span> {stat.base_stat}
+//     </li>
+//   ));
+
+//   render() {
+//     return (
+//       <div className='modal__background' onClick={this.props.handleTogglePokeModal}
+//         data-test='modal-component'
+//       >
+//         <div className='modal__body' onClick={(e) => e.stopPropagation()}
+//           data-test='modal-body'
+//         >
+//           <div className='modal__img-box'>
+//             <img 
+//               src={this.props.pokemon.sprites.front_default} 
+//               alt={this.props.pokemon.name} 
+//               className='modal__img'
+//               data-test='modal-img'
+//             />
+//           </div>
+//           <h2 className='modal__name' data-test='modal-name'>
+//             {this.props.pokemon.name}
+//          </h2>
+//           <ul className='modal__attributes'>
+//             <li className='modal__types' data-test='modal-type'>
+//               <span className='modal__stat-name'>type: </span>
+//               {this.renderTypes()}
+//             </li>
+      
+//             <li className='modal__stats'>
+//               <ul className='modal__stats-list'>
+//                 {this.renderStats()}
+//               </ul>
+//             </li>
+  
+//             <li className='modal__abilities'>
+//               <span className='modal__stat-name modal__ability-tag'>abilities: </span>
+//               <ul className='modal__abilities-list'>
+//                 {this.renderAbilities()}
+//               </ul>
+//             </li>
+//           </ul>
+//           {this.props.error && <p className='modal__error'>{this.props.error}</p>}
+//           <div className='modal__buttons'>
+//             <button 
+//               onClick={this.props.handleAddPokemon} 
+//               className='btn modal__btn'
+//               ref={this.pokeModalButton}
+//               data-test='modal-save'
+//             >
+//                Save to team
+//             </button>
+//             <button 
+//               onClick={this.props.handleTogglePokeModal} 
+//               className='btn modal__btn'
+//               data-test='modal-cancel'
+//             >
+//                Cancel
+//             </button>
+//           </div>
+//         </div>
+//       </div>   
+//     );
+//   }
+// };
+
+const Modal = ({pokemon, handleAddPokemon, handleTogglePokeModal, error}) => {
+
+  const pokeModalButton = useRef(null);
+  
+  useEffect(() => {
+    pokeModalButton.current.focus();
+  }, []);
+
+  const renderTypes = () => pokemon.types.map(type => {
     return <PokemonTypes type={type} key={type.type.name} />;
   });
   
-  renderAbilities = () => this.props.pokemon.abilities.map(ability => {
+  const renderAbilities = () => pokemon.abilities.map(ability => {
     return (
       <li 
         className='modal__ability' 
@@ -29,7 +123,7 @@ export class Modal extends React.Component{
     );
   });
 
-  renderStats = () => this.props.pokemon.stats.map(stat => (
+  const renderStats = () => pokemon.stats.map(stat => (
     <li key={stat.stat.name} data-test='modal-stat'>
       <span className='modal__stat-name'>
         { stat.stat.name }:
@@ -37,66 +131,65 @@ export class Modal extends React.Component{
     </li>
   ));
 
-  render() {
-    return (
-      <div className='modal__background' onClick={this.props.handleTogglePokeModal}
-        data-test='modal-component'
+
+  return (
+    <div className='modal__background' onClick={handleTogglePokeModal}
+      data-test='modal-component'
+    >
+      <div className='modal__body' onClick={(e) => e.stopPropagation()}
+        data-test='modal-body'
       >
-        <div className='modal__body' onClick={(e) => e.stopPropagation()}
-          data-test='modal-body'
-        >
-          <div className='modal__img-box'>
-            <img 
-              src={this.props.pokemon.sprites.front_default} 
-              alt={this.props.pokemon.name} 
-              className='modal__img'
-              data-test='modal-img'
-            />
-          </div>
-          <h2 className='modal__name' data-test='modal-name'>
-            {this.props.pokemon.name}
-         </h2>
-          <ul className='modal__attributes'>
-            <li className='modal__types' data-test='modal-type'>
-              <span className='modal__stat-name'>type: </span>
-              {this.renderTypes()}
-            </li>
-      
-            <li className='modal__stats'>
-              <ul className='modal__stats-list'>
-                {this.renderStats()}
-              </ul>
-            </li>
-  
-            <li className='modal__abilities'>
-              <span className='modal__stat-name modal__ability-tag'>abilities: </span>
-              <ul className='modal__abilities-list'>
-                {this.renderAbilities()}
-              </ul>
-            </li>
-          </ul>
-          {this.props.error && <p className='modal__error'>{this.props.error}</p>}
-          <div className='modal__buttons'>
-            <button 
-              onClick={this.props.handleAddPokemon} 
-              className='btn modal__btn'
-              ref={this.pokeModalButton}
-              data-test='modal-save'
-            >
-               Save to team
-            </button>
-            <button 
-              onClick={this.props.handleTogglePokeModal} 
-              className='btn modal__btn'
-              data-test='modal-cancel'
-            >
-               Cancel
-            </button>
-          </div>
+        <div className='modal__img-box'>
+          <img 
+            src={pokemon.sprites.front_default} 
+            alt={pokemon.name} 
+            className='modal__img'
+            data-test='modal-img'
+          />
         </div>
-      </div>   
-    );
-  }
+        <h2 className='modal__name' data-test='modal-name'>
+          {pokemon.name}
+        </h2>
+        <ul className='modal__attributes'>
+          <li className='modal__types' data-test='modal-type'>
+            <span className='modal__stat-name'>type: </span>
+            {renderTypes()}
+          </li>
+    
+          <li className='modal__stats'>
+            <ul className='modal__stats-list'>
+              {renderStats()}
+            </ul>
+          </li>
+
+          <li className='modal__abilities'>
+            <span className='modal__stat-name modal__ability-tag'>abilities: </span>
+            <ul className='modal__abilities-list'>
+              {renderAbilities()}
+            </ul>
+          </li>
+        </ul>
+        {error && <p className='modal__error'>{error}</p>}
+        <div className='modal__buttons'>
+          <button 
+            onClick={handleAddPokemon} 
+            className='btn modal__btn'
+            ref={pokeModalButton}
+            data-test='modal-save'
+          >
+              Save to team
+          </button>
+          <button 
+            onClick={handleTogglePokeModal} 
+            className='btn modal__btn'
+            data-test='modal-cancel'
+          >
+              Cancel
+          </button>
+        </div>
+      </div>
+    </div>   
+  );
 };
 
 export default Modal;
